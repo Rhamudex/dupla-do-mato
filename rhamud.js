3d87a4243bee2f44359a830ff6162a80a8668359
Rhamudex <160138944+Rhamudex@users.noreply.github.com>
2025-04-25 09:48:06 -0300

    dupla-do-mato

---
+++ rhamud.js
@@ -0,0 +1,27 @@
+let randomNumber =
+Math.floor(Math.random() * 10) +1;
+let attemps = 0;
+function checkGuess (){
+const userguess = parseInt(document.getElementById
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
